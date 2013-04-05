d45e237c14621df5cfa72ae29f20206158ceebf6
Karl Lehenbauer <karl@lehenbauer.com>
2013-04-05 07:10:38 +0000

    Improved image-playing demo, more configurable up top.

--- playpen/play_image_files.tcl
+++ playpen/play_image_files.tcl
@@ -1,6 +1,13 @@
 #
+# play_image_files - play a series of image files from the command line
+#  to the LEDs
 #
-#
+
+set delayUsecs 8000
+set nLoops 4
+set nLEDs 160
+set nRows 5000
+set delayBetweenFilesMS 500
 
 package require tclgd
 package require pilights
@@ -33,21 +40,21 @@ proc load_image {file} {
     close $fp
 
     return $cmd
-    
 }
 
-set image [load_image symmetric/400-04672846w.png]
+#set image [load_image symmetric/400-04672846w.png]
 
-set nLEDs 160
 
-pilight create lights $nLEDs 1000
+proc setup {} {
+    pilight create lights $::nLEDs $::nRows
 
-lights open /dev/spidev0.0
+    lights open /dev/spidev0.0
+}
 
 proc copy_image {gdObject piObject} {
     $piObject attach_gd $gdObject
 
-     $piObject copy_from_image 0 [$piObject nRows] 0 0 0 160
+     $piObject copy_from_image 0 [$piObject nRows] 0 0 0 [$piObject nLights]
 }
 
 proc play_images {files} {
@@ -57,19 +64,22 @@ proc play_images {files} {
         set image [load_image $file]
 	copy_image $image lights
 
-	for {set i 0} {$i < 2} {incr i} {
-	    lights write 0 [$image height] 128000
+	for {set i 0} {$i < $::nLoops} {incr i} {
+	    lights write 0 [$image height] $::delayUsecs
 	}
 	$image delete
-	after 2000
+
+	lights clear 0
+	lights write 0 1
+	after $::delayBetweenFilesMS
     }
 }
 
-#lights write 10 1 5000
+setup
 
 if {!$tcl_interactive} {
     play_images $argv
 } else {
-    puts "interactive, mess around like by doing copy_image $gd0 lights or lights write 0 100 5000"
+    puts "interactive, mess around like by doing copy_image $gd lights or lights write 0 100 5000"
 }
 
